f71d7c8c60777159f6cec0d4cc5d876dc3444db7
Harshitt23 <hasrhitr2308@gmail.com>
2025-11-20 16:48:15 +0530

    done for the noon

--- weak 4/weak 4.2/(1) TODO app using 3 ways/(3) Using React.js
+++ weak 4/weak 4.2/(1) TODO app using 3 ways/(3) Using React.js
@@ -1,10 +1,9 @@
-/*    React is Better Than Direct DOM Manipulatiom. -- 
+/*    React is Better Than Direct DOM Manipulatiom. ------------ 
             React is not replacing JavaScript.
             React is replacing YOU manually fighting with the DOM.
 
-    So doing DOm manipulation is very hard,
-    whenever we r creating a dynamic website , we will use react instead.
-----------------------------------------------------------------------
+        So doing DOm manipulation is very hard,
+        whenever we r creating a dynamic website , we will use react instead.
 
 ❌ DOM:
 Slow
@@ -24,9 +23,14 @@ Scalable for big apps
 Cleaner code
 Better developer experience
 
-----------------------------------------------------------------------
 
 React = DOM + Power ⚡
 DOM = Raw phone
-React = Smartphone with apps, features, automation
-*/
+React = Smartphone with apps, features, automation.*/
+
+
+/*----------------------------------------------------------------------
+   how to install react - mpm create vite@latest     (in cmd)
+                        - give project name 
+                        - select React
+                        - select JS                                      */
