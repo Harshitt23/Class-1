3d828a64f57fb6d3cdd76e2e721ed09870afd8c0
Harshitt23 <hasrhitr2308@gmail.com>
2025-11-30 00:15:39 +0530

    done for the day

---
+++ weak 5/(2) React TODO (weak 4.2).js
@@ -0,0 +1,6 @@
+/* So week 5 have only 2 videos , one is foundation video in which
+we have understand - react and some other things , use state and props and all.
+and we have also made an todo app which is Basic.
+
+Now in this video we will also make a todo app - but we will do in a very good client ready way !!
+its in studyand assignments folder and its named real todo of weak 5. */
